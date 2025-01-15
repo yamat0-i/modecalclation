c56169ee17ba82435ec389edac38d53132a3c897
yamat0-i <yamatoiida_61@outlook.jp>
2025-01-15 11:13:34 +0900

    Clean

--- phi-intensity.py
+++ phi-intensity.py
@@ -6,55 +6,73 @@ import fibermode
 # Parameters
 radius = 200e-9 # Fiber Radius [m]
 a = 200e-9 # Particle size [m]
+core_index = 1.45
+clad_index = 1.0
+P660 = 4.0e-3
+P785 = 3.0e-3
 
-# Polar coordinates
+# Polar coordinates(global)
 rho = radius + a/2
 phi = np.linspace(0, 2*np.pi, 1000)
 phi_deg = phi * 180/np.pi  #[deg]
 
-# Circ. pol.
-mode660 = fibermode.HE(
-    wavelength=660e-9,
-    radius=radius,
-    core_index=1.45,
-    clad_index=1.0,
-    power=4.0e-3
-    )
+def main():
+    # Circ. pol.
+    Ex660, Ey660, Ez660 = circmode(wavelnegth=660e-9,
+                                   radius=radius,
+                                   core_index=core_index,
+                                   clad_index=clad_index,
+                                   power=P660)
+    I660 = np.abs(Ex660)**2 + np.abs(Ey660)**2 + np.abs(Ez660)**2
+    #Lin. pol.
+    Ex785, Ey785, Ez785 = linearmode(wavelnegth=785e-9,
+                                     radius=radius,
+                                     core_index=core_index,
+                                     clad_index=clad_index,
+                                     power=P785,
+                                     pol=0)
+    I785 = np.abs(Ex785)**2 + np.abs(Ey785)**2 + np.abs(Ez785)**2
+    plot(I660=I660, I785=I785)
+    
+def linearmode(wavelnegth, radius, core_index, clad_index, power, pol):
+    mode = fibermode.HE(wavelength=wavelnegth,
+                        radius=radius,
+                        core_index=core_index,
+                        clad_index=clad_index,
+                        power=power)
+    Ex = mode.Ex(rho=rho, phi=phi, pol=pol)
+    Ey = mode.Ey(rho=rho, phi=phi, pol=pol)
+    Ez = mode.Ez(rho=rho, phi=phi, pol=pol)
+    return Ex, Ey, Ez
 
-Ex660H = mode660.Ex(rho, phi, pol=0)
-Ey660H = mode660.Ey(rho, phi, pol=0)
-Ez660H = mode660.Ez(rho, phi, pol=0)
+def circmode(wavelnegth, radius, core_index, clad_index, power):
+    ExH, EyH, EzH = linearmode(wavelnegth=wavelnegth,
+                     radius=radius,
+                     core_index=core_index,
+                     clad_index=clad_index,
+                     power=power,
+                     pol=0)
+    ExV, EyV, EzV = linearmode(wavelnegth=wavelnegth,
+                     radius=radius,
+                     core_index=core_index,
+                     clad_index=clad_index,
+                     power=power,
+                     pol=np.pi/2)
+    
+    Ex = (ExH + 1j*ExV) / np.sqrt(2)
+    Ey = (EyH + 1j*EyV) / np.sqrt(2)
+    Ez = (EzH + 1J*EzV) / np.sqrt(2)
+    return Ex, Ey, Ez
+    
+def plot(I660, I785):
+    plt.figure()
+    plt.plot(phi_deg, I660, color='black', label='660nm, {}mW, Circ.'.format(P660*1e3))
+    plt.plot(phi_deg, I785, color='red', label='785nm, {}mW, Lin.'.format(P785*1e3))
+    plt.xlabel(r'$\phi[\text{deg}]$')
+    plt.ylabel(r'$|E_{x}|^{2} + |E_{y}|^{2} + |E_{z}|^{2}$')
+    plt.legend()
 
-Ex660V = mode660.Ex(rho, phi, pol=np.pi/2)
-Ey660V = mode660.Ey(rho, phi, pol=np.pi/2)
-Ez660V = mode660.Ez(rho, phi, pol=np.pi/2)
+    plt.show()
 
-Ex660 = (Ex660H + 1j*Ex660V) / np.sqrt(2)
-Ey660 = (Ey660H + 1j*Ey660V) / np.sqrt(2)
-Ez660 = (Ez660H + 1J*Ez660V) / np.sqrt(2)
-
-I660 = np.abs(Ex660)**2 + np.abs(Ey660)**2 + np.abs(Ez660)**2
-
-# Lin. pol.
-mode785 = fibermode.HE(
-    wavelength=785e-9,
-    radius=radius,
-    core_index=1.45,
-    clad_index=1.0,
-    power=3.0e-3
-)
-
-Ex785 = mode785.Ex(rho, phi, pol=0)
-Ey785 = mode785.Ey(rho, phi, pol=0)
-Ez785 = mode785.Ez(rho, phi, pol=0)
-
-I785 = np.abs(Ex785)**2 + np.abs(Ey785)**2 + np.abs(Ez785)**2
-
-plt.figure()
-plt.plot(phi_deg, I660)
-plt.plot(phi_deg, I785)
-
-plt.xlabel(r'$\phi[\text{deg}]$')
-plt.ylabel(r'$|E_{x}|^{2} + |E_{y}|^{2} + |E_{z}|^{2}$')
-
-plt.show()
+if __name__ == '__main__':
+    main()
